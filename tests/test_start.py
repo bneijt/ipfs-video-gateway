0074200264d575aa1a75ed879c7345ce44bbd1ce
Bram Neijt <bram@neijt.nl>
2021-11-10 17:42:13 +0100

    Fix memory leak

--- tests/test_start.py
+++ tests/test_start.py
@@ -1,6 +1,8 @@
 
-from ipfs_video_gateway.start import is_stable
+from ipfs_video_gateway.start import is_stable, FOLDER_STATUS_CACHE
 
 def test_is_stable():
     assert is_stable("src") == False, "First time should be unstable"
     assert is_stable("src") == True, "Second time we see nothing changed"
+    is_stable("src", forget=True)
+    assert "src" not in FOLDER_STATUS_CACHE
