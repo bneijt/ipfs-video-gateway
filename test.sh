658df81f5d5608ddf20b5f3ff1b747c822168b2f
Bram Neijt <bram@neijt.nl>
2021-11-10 19:11:06 +0100

    Add docker-compose

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./build.sh
-docker run  --publish 4000:80 --volume /tmp/ipfs:/ipfs --rm -it ipfs-gateway
+docker run  --publish 4000:80 --volume /tmp/ipfs:/ipfs --rm -it ipfs-video-gateway
